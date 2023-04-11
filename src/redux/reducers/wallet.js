0a8dda25bdd7c5b5bf5f29402911916d46ae6503
Julio Rugoloe <juliorugolo@gmail.com>
2023-04-11 16:24:04 -0300

    requisito 1

--- src/redux/reducers/wallet.js
+++ src/redux/reducers/wallet.js
@@ -1,1 +1,7 @@
-// Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIAL_STATE = {
+  wallet: 0,
+};
+
+const wallet = (state = INITIAL_STATE, action) => state;
+
+export default wallet;
