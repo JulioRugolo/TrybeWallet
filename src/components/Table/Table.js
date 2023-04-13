213dea1a6b874eb616a4cac3226c767ae2d4de21
Julio Rugoloe <juliorugolo@gmail.com>
2023-04-12 21:18:43 -0300

    requisito 6 e 7

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,61 +1,72 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import './style.css';
 
-const FIXED_NUMBER = 3;
+const FIXED_NUMBER = 2;
 
 class Table extends Component {
   getExchange = (currency, index) => {
     const { expenses } = this.props;
-    const arrayExchangeRates = Object.values(expenses[index].exchangeRates);
-    const exchange = arrayExchangeRates.find((exchangeCoin) => {
-      if (exchangeCoin.code === currency) {
+    const arrayExchangeRates = Object.entries(expenses[index].exchangeRates);
+    let resultObject = {};
+    arrayExchangeRates.filter((exchangeCoin) => {
+      if (exchangeCoin[0] === currency) {
+        resultObject = { ...exchangeCoin[1] };
         return exchangeCoin;
       }
       return exchangeCoin;
     });
-    return exchange;
+    console.log(resultObject);
+    return resultObject;
   };
 
   render() {
     const { expenses } = this.props;
     return (
       <table>
-        <tr>
-          <th>Descrição</th>
-          <th>Tag</th>
-          <th>Método de pagamento</th>
-          <th>Valor</th>
-          <th>Moeda</th>
-          <th>Câmbio utilizado</th>
-          <th>Valor convertido</th>
-          <th>Moeda de conversão</th>
-          <th>Editar/Excluir</th>
-        </tr>
-
-        {expenses.map((item, index) => (
-          <tr key={ index }>
-            <td>{item.description}</td>
-            <td>{item.tag}</td>
-            <td>{item.method}</td>
-            <td>{item.value}</td>
-            <td>{item.currency}</td>
-            <td>
-              {Number(this.getExchange(item.currency, index).ask).toFixed(FIXED_NUMBER) }
-            </td>
-            <td>
-              {Number(this.getExchange(item.currency, index).ask).toFixed(FIXED_NUMBER)
-              * item.value}
-            </td>
-            <td>
-              {Number(this.getExchange(item.currency, index).ask).toFixed(FIXED_NUMBER)
-              * item.value}
-            </td>
-            <td>
-              {this.getExchange(item.currency, index).codein }
-            </td>
-
+        <thead>
+          <tr className="header">
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
           </tr>
-        ))}
+        </thead>
+        <tbody>
+          {expenses.map((item, index) => (
+            <tr key={ index }>
+              <td>{item.description}</td>
+              <td>{item.tag}</td>
+              <td>{item.method}</td>
+              <td>{Number(item.value).toFixed(2)}</td>
+              <td>{this.getExchange(item.currency, index).name}</td>
+              <td>
+                {
+                  parseFloat(this.getExchange(item.currency, index).ask)
+                    .toFixed(FIXED_NUMBER)
+                }
+              </td>
+              <td>
+                {
+                  (parseFloat(this.getExchange(item.currency, index).ask)
+                * parseFloat(item.value)).toFixed(FIXED_NUMBER)
+                }
+              </td>
+              <td>Real</td>
+              <td>
+                <button>Editar</button>
+                <button>Excluir</button>
+              </td>
+
+            </tr>
+          ))}
+        </tbody>
 
       </table>
     );
